410385accca273c12c545ebd4d4b3c4c57f3ca3a
sophiaboss11 <bosssophiaboss@gmail.com>
2020-07-12 15:46:03 -0700

    Project upgrade to URP + file organization!

---
+++ Wasteland_Summer/Assets/_Scripts/SetHoseState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetHoseState : MonoBehaviour
+{
+    public bool active = false;
+    GameObject player;
+    HoseState hoseState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var Players = GameObject.FindGameObjectsWithTag("Player");
+        foreach(GameObject playerObject in Players)
+        {
+            hoseState = playerObject.GetComponentInChildren<HoseState>();
+            if(hoseState != null)
+            {
+                hoseState.SetHoseActive(active);
+            }
+            
+        }
+//        player = GameObject.FindGameObjectWithTag("Player");
+//        hoseState = player.GetComponent<HoseState>();
+//        hoseState.SetHoseActive(active);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
